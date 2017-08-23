6a7c4766617df3b3bfd42596060fc6662d00d66a
maratnek <maratn566ek@gmail.com>
2017-08-23 16:59:17 +0300

    all commit catalog form

--- src/app/components/catalog/catalog.component.ts
+++ src/app/components/catalog/catalog.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, NgControl } from '@angular/forms';
+import 'rxjs/add/operator/startWith';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-catalog',
@@ -7,15 +10,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CatalogComponent implements OnInit {
 
+  isSuppliersCheck = false;
+
   measures = [
     {name: "Вес", ms: "кг."},
     {name: "Длина", ms: "метр"},
     {name: "Количество", ms: "шт."}
-  ]
+  ];
+  catalog: any = {};
 
-  constructor() { }
+  stateCtrl: FormControl;
+  filteredStates: any;
+  states = [
+    'Alabama',
+    'Alaska'
+  ];
+
+  constructor() {
+    this.stateCtrl = new FormControl();
+    this.filteredStates = this.stateCtrl.valueChanges
+      .startWith(null)
+      .map(nm => this.filterStates(nm));
+  }
 
   ngOnInit() {
+  }
+
+  onSubmit(addNewCatalog) {
+    console.log(this.catalog);
+  }
+
+  storeSectore(){
+    let data = [];
+    let str = new String("ABCD");
+    console.log(str.charAt(3));
+    // for (let i : String = "A"; i < "Z"; i++) {
+    //   console.log(i);
+    //   data.push(i);
+    // }
+    return data;
+  }
+
+  filterStates(val: string) {
+    return val ? this.states.filter(s => s.toLowerCase().indexOf(val.toLowerCase()) === 0)
+    : this.states;
   }
 
 }
